1b703422704f1f765aaf78e5a477127e905fadd2
Maxime Kabel <maxime.kabel001@gmail.com>
2020-07-16 15:54:02 +0200

    Changed pip3 back to pip

--- server/update.sh
+++ server/update.sh
@@ -3,6 +3,6 @@ cd /home/prod/Cosmos
 git checkout master
 git pull
 source .venv/bin/activate
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 sudo systemctl restart cosmos-website.service

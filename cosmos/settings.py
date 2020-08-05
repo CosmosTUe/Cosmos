b5df65579b3670c013de39a1b3fb775eebc68544
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2020-08-05 11:23:09 +0200

    Cleanup static

--- cosmos/settings.py
+++ cosmos/settings.py
@@ -92,14 +92,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(DATA_DIR, "media")
 STATIC_ROOT = os.path.join(DATA_DIR, "static")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "cosmos", "static"),)
 SITE_ID = 1
-
-STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-)
-
 
 TEMPLATES = [
     {

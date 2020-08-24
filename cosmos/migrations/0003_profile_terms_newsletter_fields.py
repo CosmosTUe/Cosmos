be62daaf4f85ef0fd132ac7009e05b9f7167bd69
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2020-08-24 11:04:33 +0200

    Fix migrations and css

--- cosmos/migrations/0003_profile_terms_newsletter_fields.py
+++ cosmos/migrations/0003_profile_terms_newsletter_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("cosmos", "0001_init_models"),
+        ("cosmos", "0002_photo_fields"),
     ]
 
     operations = [

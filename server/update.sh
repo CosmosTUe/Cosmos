b2ec4241aac4710e527f5ae54fabf6753a0bf7f5
Maxime Kabel <maxime.kabel001@gmail.com>
2020-09-08 11:57:01 +0200

    Removed server files

--- server/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /home/prod/Cosmos
-git checkout master
-git pull
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-sudo systemctl restart cosmos-website.service

48ec976a10240a418a96e1ca7816bd785cdc600e
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2020-09-29 09:43:22 +0200

    Remove debug statements

--- apps/legacy/admin.py
+++ apps/legacy/admin.py
@@ -1,10 +1,7 @@
-import time
-
 from django.contrib import admin
 
 from apps.legacy.models import MysiteProfile
 from apps.legacy.tasks import send_migration_emails_task
-from cosmos.celery import app
 
 
 @admin.register(MysiteProfile)
@@ -21,10 +18,7 @@ class LegacyProfileAdmin(admin.ModelAdmin):
     get_username.admin_order_field = "user__username"
 
     def send_migration_email(self, request, queryset):
-        result = send_migration_emails_task.delay([u for u in queryset.values_list("id", flat=True)])
-        for i in range(1, 10):
-            print(app.backend.get_result(result.id))
-            time.sleep(1)
+        send_migration_emails_task.delay([u for u in queryset.values_list("id", flat=True)])
         self.message_user(request, f"{len(queryset)} emails sent!")
 
     send_migration_email.short_description = "Send migration email"

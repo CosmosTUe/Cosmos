0314b5b69c1555a61c03dfe0812716f4c87ab62d
Maxime Kabel <maxime.kabel001@gmail.com>
2020-10-02 10:07:31 +0200

    Revert "document apps.py"
    This reverts commit 60ea7b3c3ae7b66c031c3a79f474a5612952aa1a.

--- apps/cms_plugins/apps.py
+++ apps/cms_plugins/apps.py
@@ -1,6 +1,3 @@
-#this file declares the new app cms_plugins, which is used to add new (small) plugins to the cms
-#It is mainly used for the congfiguration of the app
-
 from django.apps import AppConfig
 
 

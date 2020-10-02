60ea7b3c3ae7b66c031c3a79f474a5612952aa1a
Maxime Kabel <maxime.kabel001@gmail.com>
2020-10-02 10:07:31 +0200

    document apps.py

--- apps/cms_plugins/apps.py
+++ apps/cms_plugins/apps.py
@@ -1,3 +1,6 @@
+#this file declares the new app cms_plugins, which is used to add new (small) plugins to the cms
+#It is mainly used for the congfiguration of the app
+
 from django.apps import AppConfig
 
 

b3683039742f6eeadecdfc81f4e59c5a2d12640f
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2020-10-02 19:46:08 +0200

    Fix password reset view

--- apps/legacy/urls.py
+++ apps/legacy/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "cosmos_legacy"
 
 urlpatterns = [
-    path("accounts/import/<uidb64>/<token>/", views.import_user, name="import_user"),
+    path("import/<uidb64>/<token>/", views.import_user, name="import_user"),
 ]
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import include
 from django.urls import path
 
 from . import views
@@ -9,5 +8,4 @@ urlpatterns = [
     path("accounts/register/", views.register, name="user_register"),
     path("accounts/profile/", views.profile, name="user_profile"),
     path("accounts/delete", views.delete, name="user_delete"),
-    path("accounts/", include("django.contrib.auth.urls")),
 ]

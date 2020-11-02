b55c8124b165d1b842d238c5a3badfdf9301f51a
Danang Rahmatullah <dasyad00@gmail.com>
2020-11-02 07:15:22 +0100

    Add and remove users from subscription list

--- apps/users/newsletter.py
+++ apps/users/newsletter.py
@@ -14,13 +14,28 @@ import json
 
 import sendgrid
 
+from apps.users.models import Profile
 from cosmos import settings
 
 sg = sendgrid.SendGridAPIClient(api_key=settings.EMAIL_HOST_PASSWORD)
 
 
+# TODO consider removing from newsletter list, not from contacts
+def __get_list_id(list_name):
+    # https://sendgrid.api-docs.io/v3.0/lists/get-all-lists
+    response = sg.client.marketing.lists.get()
+    data = json.loads(response.body.decode("utf-8"))
+    return data["result"][0]["id"]
+
+
+__newsletter_id = __get_list_id("Newsletter")
+
+
 def is_subscribed(email):
     # https://sendgrid.api-docs.io/v3.0/contacts/search-contacts
+    # TODO consider removing from newsletter list, not from contacts
+    # response = sg.client.marketing.contacts.search.post(request_body={
+    #     "query": f"email LIKE '{email}%%' AND CONTAINS(list_ids, '{__newsletter_id}')"})
     response = sg.client.marketing.contacts.search.post(request_body={"query": f"email LIKE '{email}%%'"})
     data = json.loads(response.body.decode("utf-8"))
     matches = data["contact_count"]
@@ -31,6 +46,36 @@ def is_subscribed(email):
     return matches == 1
 
 
-if __name__ == "__main__":
-    print(f"max is {is_subscribed('max')}")
-    print(f"dng is {is_subscribed('dasyad00')}")
+def __get_user_id(profile: Profile):
+    # https://sendgrid.api-docs.io/v3.0/contacts/search-contacts
+    response = sg.client.marketing.contacts.search.post(request_body={"query": f"email LIKE '{profile.username}'"})
+    data = json.loads(response.body.decode("utf-8"))
+    if data["contact_count"] == 0:
+        return None
+
+    if data["contact_count"] > 1:
+        print("WARNING: Duplicate emails registered")
+
+    # assumes user is first on the list
+    return data["result"][0]["id"]
+
+
+def add_subscription(profile: Profile):
+    # https://sendgrid.api-docs.io/v3.0/contacts/add-or-update-a-contact
+    response = sg.client.marketing.contacts.put(
+        request_body={
+            "contacts": [
+                {"email": profile.username, "first_name": profile.user.first_name, "last_name": profile.user.last_name}
+            ]
+        }
+    )
+
+    # return whether removal was successful
+    return response.status_code == 202
+
+
+def remove_subscription(profile: Profile):
+    # https://sendgrid.api-docs.io/v3.0/contacts/delete-contacts
+    response = sg.client.marketing.contacts.delete(query_params={"ids": __get_user_id(profile)})
+    # return whether removal was successful
+    return response.status_code == 202

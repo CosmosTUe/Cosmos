47ea46061f6557f1d79c79f0710dc48f111659d1
Maxime Kabel <maxime.kabel001@gmail.com>
2021-02-28 11:56:04 +0100

    Fix formatting

--- cosmos/settings.py
+++ cosmos/settings.py
@@ -264,42 +264,42 @@ THUMBNAIL_PROCESSORS = (
 # https://docs.djangoproject.com/en/3.1/topics/logging/
 LOGGING_FOLDER = secret_settings.secrets["LOGGING"]["FOLDER"]  # relative to root of project
 LOGGING = {
-   "version": 1,
-   "disable_existing_loggers": False,
-   "filters": {
-       "require_debug_false": {
-           "()": "django.utils.log.RequireDebugFalse",
-       },
-       "require_debug_true": {
-           "()": "django.utils.log.RequireDebugTrue",
-       },
-   },
-   "formatters": {
-       # copied from DEFAULT_LOGGING https://github.com/django/django/blob/master/django/utils/log.py
-       "verbose": {"()": "django.utils.log.ServerFormatter", "format": "[{server_time}] {message}", "style": "{"}
-   },
-   "handlers": {
-       "console": {"class": "logging.StreamHandler", "filters": ["require_debug_true"], "formatter": "verbose"},
-       "file": {
-           "class": "logging.handlers.TimedRotatingFileHandler",
-           "when": "midnight",
-           "interval": 1,
-           "filename": os.path.join(LOGGING_FOLDER, "debug.log"),
-           "formatter": "verbose",
-       },
-       "mail_admins": {
-           "level": "ERROR",
-           "filters": ["require_debug_false"],
-           "class": "django.utils.log.AdminEmailHandler",
-       },
-   },
-   "loggers": {
-       "django": {
-           "handlers": ["file", "console", "mail_admins"],
-           "level": "INFO",
-           "propagate": True,
-       },
-   },
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        # copied from DEFAULT_LOGGING https://github.com/django/django/blob/master/django/utils/log.py
+        "verbose": {"()": "django.utils.log.ServerFormatter", "format": "[{server_time}] {message}", "style": "{"}
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "filters": ["require_debug_true"], "formatter": "verbose"},
+        "file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "when": "midnight",
+            "interval": 1,
+            "filename": os.path.join(LOGGING_FOLDER, "debug.log"),
+            "formatter": "verbose",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file", "console", "mail_admins"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
 }
 
 # Email

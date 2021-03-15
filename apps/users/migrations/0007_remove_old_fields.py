ad89e777cd1e528561dd3c4321120a003916a88f
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2021-03-15 19:26:40 +0100

    Add migration for user revamp

--- apps/users/migrations/0007_remove_old_fields.py
+++ apps/users/migrations/0007_remove_old_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("users", "0004_remove_profile_status"),
+        ("users", "0006_institutionfontys_institutiontue"),
     ]
 
     operations = [

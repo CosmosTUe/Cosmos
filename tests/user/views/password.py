e19b2ba0623946808abe57ac02369e8b026a5967
Danang Rahmatullah <dasyad00@gmail.com>
2021-07-25 08:45:29 +0200

    test for working password reset view

--- tests/user/views/password.py
+++ tests/user/views/password.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
 from django.core import mail
 from django.test import TestCase
@@ -22,6 +23,23 @@ class PasswordResetViewTest(TestCase):
         self.assertEqual(mail.outbox[0].to[0], exp_email_recipient)
         self.assertEqual(mail.outbox[0].from_email, exp_email_sender)
         self.assertEqual(mail.outbox[0].subject, exp_email_subject)
+        self.assert_working_password_reset_view()
+
+    def assert_working_password_reset_view(self):
+        # setup
+        html_parser = BeautifulSoup(mail.outbox[0].body, "html.parser")
+        link = html_parser.find("a")
+        link_url = link.get("href")
+
+        exp_status_code = 302
+        exp_url = "/accounts/reset/MQ/set-password/"
+
+        # act
+        response = self.client.get(link_url)
+
+        # test
+        self.assertEqual(exp_status_code, response.status_code)
+        self.assertEqual(exp_url, response.url)
 
     def test_success(self):
         # setup

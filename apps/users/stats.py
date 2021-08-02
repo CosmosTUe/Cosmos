2b7e195a7bff250eea82f83ee244e7a972d29aef
Maxime Kabel <maxime.kabel001@gmail.com>
2021-08-03 00:00:44 +0200

    Added stats button to institution page

--- apps/users/stats.py
+++ apps/users/stats.py
@@ -5,20 +5,29 @@ from django.db.models import Count
 # dict = {nat[item]['nationality']:nat[item]['nationality__count'] for item in data]
 
 
-def get_stats(query):
+def get_nationality_stats(query):
 
     # Creates a list of dictionaries of the nationalities and their count
+    nationality_list = query.values("nationality").annotate(Count("nationality"))
+
+    # Turn the list of dictionaries into one dictionary
+    nationality = {item["nationality"]: item["nationality__count"] for item in nationality_list}
+
+    # Call the plotting function (used in case the plotting function wants to be changed)
+    plots = [make_plot(nationality, "nationality")]
+    return plots
+
+
+def get_major_stats(query):
     department_list = query.values("department").annotate(Count("department"))
     program_list = query.values("program").annotate(Count("program"))
-    nationality_list = query.values("nationality").annotate(Count("nationality"))
 
     # Turn the list of dictionaries into one dictionary
     department = {item["department"]: item["department__count"] for item in department_list}
     program = {item["program"]: item["program__count"] for item in program_list}
-    nationality = {item["nationality"]: item["nationality__count"] for item in nationality_list}
 
     # Call the plotting function (used in case the plotting function wants to be changed)
-    plots = [make_plot(department, "department"), make_plot(program, "program"), make_plot(nationality, "nationality")]
+    plots = [make_plot(department, "department"), make_plot(program, "program")]
     return plots
 
 

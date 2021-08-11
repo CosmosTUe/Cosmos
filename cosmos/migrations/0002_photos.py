0e6c903d6ee8d8c9e589bb0bf5a7479ff26ddba6
Maxime Kabel <maxime.kabel001@gmail.com>
2021-08-11 11:48:09 +0200

    Moved migrations for news to allow recreation

--- cosmos/migrations/0002_photos.py
+++ cosmos/migrations/0002_photos.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("cosmos", "0002_news"),
+        ("cosmos", "0001_initial"),
     ]
 
     operations = [

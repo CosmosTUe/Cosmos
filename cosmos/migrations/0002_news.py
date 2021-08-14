e71a73b90f8b8b41f3f0c03535b014036d997593
Maxime Kabel <maxime.kabel001@gmail.com>
2021-08-14 22:15:42 +0200

    Renamed Migrations

--- cosmos/migrations/0002_news.py
+++ cosmos/migrations/0002_news.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("cosmos", "0002_photos"),
+        ("cosmos", "0001_initial"),
     ]
 
     operations = [

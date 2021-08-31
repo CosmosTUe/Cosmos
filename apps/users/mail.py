d502e8c13686fba2835ec30a60d32ee281bc8888
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2021-08-31 17:23:42 +0200

    Send email via sendgrid

--- apps/users/mail.py
+++ apps/users/mail.py
@@ -1,4 +1,4 @@
-from django.core.mail import EmailMessage
+from sendgrid.helpers.mail import Mail
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
@@ -7,7 +7,7 @@ from .models import Profile
 from .tokens import account_activation_token
 
 
-def create_confirm_account_email(profile: Profile) -> EmailMessage:
+def create_confirm_account_email(profile: Profile) -> Mail:
     mail_subject = "[Cosmos] Confirm your email address"
     message = render_to_string(
         "user/mail_confirmation.html",
@@ -17,15 +17,15 @@ def create_confirm_account_email(profile: Profile) -> EmailMessage:
             "token": account_activation_token.make_token(profile.user),
         },
     )
-    email = EmailMessage(
-        mail_subject,
-        message,
+    email = Mail(
+        subject=mail_subject,
+        html_content=message,
         from_email="noreply@cosmostue.nl",
-        to=[profile.user.username],
+        to_emails=profile.user.username,
         # List-Unsubscribe headers allows for easy unsubscribing from the mailing list by sending an email to
         # the specified email automatically. Is needed to not be classified as spam, and very import to handle
         # any requests correctly.
-        headers={"List-Unsubscribe": "mailto:webcom@cosmostue.nl"},
+        #headers={"List-Unsubscribe": "mailto:webcom@cosmostue.nl"},
     )
-    email.content_subtype = "html"
+    #email.content_subtype = "html"
     return email
--- tests/user/views/registration.py
+++ tests/user/views/registration.py
@@ -26,10 +26,10 @@ class RegistrationFlowTest(WizardViewTestCase):
         executor.execute()
 
         # test
-        self.assertEqual(len(mail.outbox), 1, "1 message sent")
-        self.assertEqual(mail.outbox[0].to[0], exp_email_recipient)
-        self.assertEqual(mail.outbox[0].from_email, exp_email_sender)
-        self.assertEqual(mail.outbox[0].subject, exp_email_subject)
+        self.assertEqual(len(self.newsletter_service.outbox), 1, "1 message sent")
+        self.assertEqual(self.newsletter_service.outbox[0].personalizations[0].tos[0]["email"], exp_email_recipient)
+        self.assertEqual(self.newsletter_service.outbox[0].from_email.email, exp_email_sender)
+        self.assertEqual(self.newsletter_service.outbox[0].subject.subject, exp_email_subject)
         self.assert_working_activation_view()
 
     def assert_no_email_sent(self):
@@ -37,7 +37,7 @@ class RegistrationFlowTest(WizardViewTestCase):
 
     def assert_working_activation_view(self):
         # setup
-        html_parser = BeautifulSoup(mail.outbox[0].body, "html.parser")
+        html_parser = BeautifulSoup(self.newsletter_service.outbox[0].contents[0].content, "html.parser")
         link = html_parser.find("a")
         link_text = link.contents[0].strip()
         link_url = link.get("href")

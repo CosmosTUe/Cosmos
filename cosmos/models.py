8e7488ed3373d015e90c6b4359b5cf0d584c278c
Maxime Kabel <maxime.kabel001@gmail.com>
2021-09-10 00:01:09 +0200

    Event model

--- cosmos/models.py
+++ cosmos/models.py
@@ -2,7 +2,7 @@ import datetime
 
 from ckeditor.fields import RichTextField
 from crum import get_current_user
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.db import models
 from django.urls import reverse
 
@@ -100,6 +100,18 @@ class News(models.Model):
 
     def __str__(self):
         return "News: {" + self.title + ", " + str(self.publish_date) + "}"
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="events")
+    description = RichTextField()
+    lead = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    member_only = models.ForeignKey
+    location = models.CharField()
+    organizer = models.ForeignKey(Group, blank=True, on_delete=models.CASCADE)
 
 
 class Testimonial(models.Model):

8cbea27fbd097809c5b263f8f9b79a7af0e3e24a
crackerOil <vladimirr.lazar@gmail.com>
2023-05-15 12:00:42 +0200

    Make testimonials nice

--- apps/core/models/core.py
+++ apps/core/models/core.py
@@ -1,7 +1,17 @@
+import math
+
+from django.core.validators import ValidationError
 from django.db import models
 
 
+def validate_aspect_ratio(image):
+    ratio = 1
+    if not math.isclose(image.width / image.height, ratio, rel_tol=1e-6):
+        raise ValidationError("The aspect ratio is not correct. The aspect ratio should be: " + str(ratio))
+
+
 class Testimonial(models.Model):
+    image = models.ImageField(upload_to="testimonials", blank=True, validators=[validate_aspect_ratio])
     text = models.TextField(blank=False)
     author = models.CharField(blank=False, max_length=100)
 

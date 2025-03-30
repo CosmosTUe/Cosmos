bdc12b7d542f3bced6a6d036bcf43d4175a6242a
eduardo-devintage <63951752+eduardo-devintage@users.noreply.github.com>
2025-03-30 14:19:46 +0200

    datetime picker added if document already loaded

--- apps/events/static/cosmos_events/js/events.js
+++ apps/events/static/cosmos_events/js/events.js
@@ -1,9 +1,18 @@
-window.addEventListener('load', (event) => {
-    console.log("event-outside-log")
+if (document.readyState == "complete") {
     if (document.getElementById("id_start_date_time")) {
         flatpickr("#id_start_date_time", { enableTime: true, dateFormat: "Y-m-d H:i", time_24hr: true, });
     }
     if (document.getElementById("id_end_date_time")) {
         flatpickr("#id_end_date_time", { enableTime: true, dateFormat: "Y-m-d H:i", time_24hr: true, });
     }
-});
+} else{
+    window.addEventListener('load', (event) => {
+        console.log("event-outside-log")
+        if (document.getElementById("id_start_date_time")) {
+            flatpickr("#id_start_date_time", { enableTime: true, dateFormat: "Y-m-d H:i", time_24hr: true, });
+        }
+        if (document.getElementById("id_end_date_time")) {
+            flatpickr("#id_end_date_time", { enableTime: true, dateFormat: "Y-m-d H:i", time_24hr: true, });
+        }
+    });
+}

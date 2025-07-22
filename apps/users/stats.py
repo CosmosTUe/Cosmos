154a40b55b4e7861b01aaadcd40daf23409dbdff
karabalalala <167786634+karabalalala@users.noreply.github.com>
2025-07-23 01:51:23 +0300

    implemented getting stats for academic year joined
    as well as distribution of bachelor/master/exchange and eea/non-eea students. this is in place of the nationality stats button. but that old code is still kept. also 'tmp' folder where graph jpg was kept was changed to 'temp' since windows didn't like that, shouldn't be an issue

--- apps/users/stats.py
+++ apps/users/stats.py
@@ -1,6 +1,50 @@
 import matplotlib.pyplot as plt
-from django.db.models import Count
+from django.db.models import Count, Case, When, Value, BooleanField, F, CharField
+from django.db.models.functions import ExtractMonth, ExtractYear, Concat
 
+from apps.users.models.profile import Profile
+
+def get_date_joined_stats(query):
+    EU_NATIONALITIES = ["Austrian", "Belgian", "Bulgarian", "Croatian", "Cypriot", "Czech",
+                        "Danish", "Dutch", "Estonian", "Finnish", "French", "German", "Greek",
+                        "Hungarian", "Icelander", "Irish", "Italian", "Latvian", "Liechtensteiner",
+                        "Lithuanian", "Luxembourger", "Maltese", "Norwegian", "Polish", "Portuguese",
+                        "Romanian", "Slovakian", "Slovenian", "Spanish", "Swedish"]
+
+
+    # Takes an academic year as July 1-June 30th, coinciding with a board year,
+    # makes a list with other relevant data
+    date_joined_list = query.annotate(month=ExtractMonth('user__date_joined'), year=ExtractYear('user__date_joined'),
+            academic_start_year=Case(
+                When(month__gte=7, then=F('year')),                     
+                default=F('year') - 1                                   
+            ),
+            academic_end_year=Case(
+                When(month__gte=7, then=F('year') + 1),
+                default=F('year')
+            ),
+            academic_year=Concat(
+                F('academic_start_year'),
+                Value('–'),
+                F('academic_end_year'),
+                output_field=CharField()
+            ),
+            is_eu=Case(
+                When(nationality__in=EU_NATIONALITIES, then=Value(True)),
+                default=Value(False),
+                output_field=BooleanField()
+            )
+        ).values('academic_year', 'institutiontue__program', 'is_eu'
+                 ).annotate(count=Count('id')
+                            ).order_by('academic_year', 'institutiontue__program', 'is_eu')
+
+    # Turn the list of dictionaries into one dictionary
+    date_joined = {f"{item['academic_year']} | {item['institutiontue__program']} | {'EU' if item['is_eu']else 'Non-EU'}": item['count']
+                   for item in date_joined_list}
+
+    # Call the plotting function (used in case the plotting function wants to be changed)
+    plots = [make_plot(date_joined, 'AON wanted this data ONLY TUE STUDENTS HERE tho tbf thats by far the vast majority')]
+    return plots
 
 def get_nationality_stats(query):
     # Creates a list of dictionaries of the nationalities and their count
@@ -29,7 +73,7 @@ def get_major_stats(query):
 
 def make_plot(data, name):
     # Makes a plot from the data and then returns the location of where it's saved
-    filepath = "/tmp/" + name + "-graph.jpg"
+    filepath = "/temp/" + name + "-graph.jpg"
     plt.figure(1, [20, 8])
     rects = plt.bar(data.keys(), data.values())
     plt.title(name.capitalize())

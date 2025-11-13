f11d817559ef32669b9be20e789fc18869708fe8
Ádám Hidegkuti <adam.hidegkuti@gmail.com>
2025-11-13 12:36:08 +0100

    Revert "Partners animation fix"
    This reverts commit f5e69ceabd8a5346f8993835fe2878c948475c1e.

---
+++ apps/core/static/cosmos/js/about.js
@@ -0,0 +1,18 @@
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    if (window.matchMedia("(min-width: 992px)").matches)
+    {
+        const minPerSlide = 3
+        let next = el.nextElementSibling
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    }
+})

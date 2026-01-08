1dbf59a049db78984a6be2bc88159829997ae1b5
Ádám Hidegkuti <adam.hidegkuti@gmail.com>
2026-01-08 13:32:16 +0100

    Remove "subscribe to newsletter" button when ths user is already subscribed

--- apps/core/views/news.py
+++ apps/core/views/news.py
@@ -1,9 +1,11 @@
 import datetime
 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, UpdateView
+from newsletter.models import Newsletter, Subscription
 
 from apps.core.forms.news import NewsForm
 from apps.core.models.news import News
@@ -68,7 +70,32 @@ def news_list(request):
         news_list = News.objects.order_by("-publish_date").all()
     else:
         news_list = News.objects.filter(publish_date__lte=datetime.date.today()).order_by("-publish_date").all()
+    
+    # Check if user is subscribed to BOTH newsletters
+    is_subscribed = False
+    if request.user.is_authenticated:
+        try:
+            cosmos_news = Newsletter.objects.get(slug__exact="cosmos-news")
+            gmm = Newsletter.objects.get(slug__exact="gmm")
+            
+            cosmos_subscribed = Subscription.objects.filter(
+                Q(email_field=request.user.username) | Q(email_field=request.user.email),
+                newsletter=cosmos_news,
+                subscribed=True
+            ).exists()
+            
+            gmm_subscribed = Subscription.objects.filter(
+                Q(email_field=request.user.username) | Q(email_field=request.user.email),
+                newsletter=gmm,
+                subscribed=True
+            ).exists()
+            
+            is_subscribed = cosmos_subscribed and gmm_subscribed
+        except Newsletter.DoesNotExist:
+            is_subscribed = False
+    
     context = {
         "news_list": news_list,
+        "is_subscribed": is_subscribed,
     }
     return render(request, "news/news_list.html", context)
